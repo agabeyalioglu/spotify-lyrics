f293e440d7b8d327083539bac22c18c00ba09600
Agabey Alioglu <agabeyalioglu@gmail.com>
2018-08-05 14:11:02 +0300

    Updated fetch_lyrics

--- src/fetch_lyrics.py
+++ src/fetch_lyrics.py
@@ -5,9 +5,5 @@ def fetch_lyrics(song_artist, song_title):
     quote_page = 'http://www.metrolyrics.com/take-me-to-church-lyrics-hozier.html'
     page = requests.get(quote_page)
     soup = BeautifulSoup(page.text, 'html.parser')
-    lyrics = soup.findAll("p", class_='verse')
-
-    untidy_text = '\n\n'.join(str(i) for i in lyrics)
-    lyrics = re.sub(r"<.*?>", "", untidy_text)
-
-    return lyrics
+    
+    return "\n\n".join(i.text for i in soup.findAll("p", class_='verse'))
